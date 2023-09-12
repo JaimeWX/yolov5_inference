4f16b2321f251906d3e8578a203090c43e2d7255
jaime_westwell <1261360793@qq.com>
2023-09-12 11:34:05 +0800

    yolov5 cpp inference

---
+++ build/CMakeFiles/yolov5_cv.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jaime/wwws/yolov5_inference/yolov5_cv.cpp" "/home/jaime/wwws/yolov5_inference/build/CMakeFiles/yolov5_cv.dir/yolov5_cv.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jaime/opencv-4.5.5/build"
+  "/home/jaime/opencv-4.5.5/include"
+  "/home/jaime/opencv-4.5.5/modules/core/include"
+  "/home/jaime/opencv-4.5.5/modules/flann/include"
+  "/home/jaime/opencv-4.5.5/modules/imgproc/include"
+  "/home/jaime/opencv-4.5.5/modules/ml/include"
+  "/home/jaime/opencv-4.5.5/modules/photo/include"
+  "/home/jaime/opencv-4.5.5/modules/dnn/include"
+  "/home/jaime/opencv-4.5.5/modules/features2d/include"
+  "/home/jaime/opencv-4.5.5/modules/imgcodecs/include"
+  "/home/jaime/opencv-4.5.5/modules/videoio/include"
+  "/home/jaime/opencv-4.5.5/modules/calib3d/include"
+  "/home/jaime/opencv-4.5.5/modules/highgui/include"
+  "/home/jaime/opencv-4.5.5/modules/objdetect/include"
+  "/home/jaime/opencv-4.5.5/modules/stitching/include"
+  "/home/jaime/opencv-4.5.5/modules/ts/include"
+  "/home/jaime/opencv-4.5.5/modules/video/include"
+  "/home/jaime/opencv-4.5.5/modules/gapi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/yolov5_cv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov5_cv.dir/yolov5_cv.cpp.o"
+  "yolov5_cv"
+  "yolov5_cv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov5_cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
